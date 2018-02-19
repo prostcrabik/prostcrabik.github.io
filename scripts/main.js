8cda7be903af76769f3d223851c321d4de51e524
Pavel <36544569+prostcrabik@users.noreply.github.com>
2018-02-19 16:34:59 +0300

    Yes... changes

--- scripts/main.js
+++ scripts/main.js
@@ -8,22 +8,3 @@ myImage.onclick = function() {
       myImage.setAttribute ('src','images/yJSJYmN1IQs.jpg');
     }
 }
-var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
-
-function setUserName() {
-	var myName = prompt('Пожалуйста введите свое имя.'); //prompt Вызывает диалоговое окно и просит ввести некоторые джанные
-	localStorage.setItem('name', myName); //localStorage позволяет сохранять данные в браузере
-	myHeading.innerHTML = 'Test, ' + myName;
-}
-
-if(!localStorage.getItem('name')) {
-	setUserName();
-} else {
-	var storedName = localStorage.getItem('name');
-	myHeading.innerHTML = 'Test, ' + storedName;
-}
-
-myButton.onclick = function() {
-	setUserName();
-}
